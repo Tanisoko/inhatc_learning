f250c083d4cb45f465794663c3292d7deb9e0ad2
Tanisoko <jy0317310@naver.com>
2023-09-25 15:50:57 +0900

    week05 v0.06

--- week05/main.go
+++ week05/main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	dice := rand.Intn(6) + 1
-	fmt.Println(dice)
+	rand.Seed(time.Now().Unix()) //	get the current data and time as an integer
+	answer := rand.Intn(100) + 1 //	random integer number 1 ~ 100
+	fmt.Println("Guess Number Game!")
+	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("You have", 10-i, "chances")
+		fmt.Print("Input Guess Number : ")
+		inputNumberString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		inputNumberString = strings.TrimSpace(inputNumberString)
+		inputNumber, err := strconv.Atoi(inputNumberString)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if inputNumber < answer {
+			fmt.Println("Guess number is lower than answer")
+		} else if inputNumber > answer {
+			fmt.Println("Guess number is bigger than answer")
+		} else if inputNumber == answer {
+			fmt.Println("Correct!")
+		} else {
+			fmt.Println("I think you input the wrong data")
+		}
+	}
 }
