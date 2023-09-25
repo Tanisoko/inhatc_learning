3d5d06f119ecb1d53677ef2840041283f940844f
Tanisoko <jy0317310@naver.com>
2023-09-25 14:36:54 +0900

    week05 v0.02 README.MD

--- week05/main.go
+++ week05/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
@@ -11,9 +12,14 @@ func main() {
 	//	var month string = now.Month().String()
 	//	fmt.Println(year, month, now.Hour(), now.Minute(), now.Second())
 
-	Hotspurs := "gm ? j madi?"
-	replacePlayer := strings.NewReplacer("?", "son")
-	Player := replacePlayer.Replace(Hotspurs)
-	fmt.Println(Player)
+	//	Hotspurs := "gm ? j madi?"
+	//	replacePlayer := strings.NewReplacer("?", "son")
+	//	Player := replacePlayer.Replace(Hotspurs)
+	//	fmt.Println(Player)
+
+	fmt.Print("Input Score : ")
+	reader := bufio.NewReader(os.Stdin)
+	inputScore, err := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
+	fmt.Println(inputScore, err)
 
 }
