b01087615496689f0010b454cc759f29e36ee18e
Tanisoko <jy0317310@naver.com>
2023-09-25 15:23:20 +0900

    week05 v0.05

--- week05/main.go
+++ week05/main.go
@@ -5,32 +5,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	//	var now time.Time = time.Now()
-	//	var year int = now.Year()
-	//	var month string = now.Month().String()
-	//	fmt.Println(year, month, now.Hour(), now.Minute(), now.Second())
-
-	//	Hotspurs := "gm ? j madi?"
-	//	replacePlayer := strings.NewReplacer("?", "son")
-	//	Player := replacePlayer.Replace(Hotspurs)
-	//	fmt.Println(Player)
 
 	fmt.Print("Input Score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n')
-	//	inputScore, _ := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
+	inputScoreString, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if inputScore >= 90 { //	mismatched types string and untyped int
-		grade := "A grade!"
+	inputScoreString = strings.TrimSpace(inputScoreString)
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32)
+
+	var grade string
+
+	if inputScore >= 90 {
+		grade = "A grade!"
 	} else {
-		grade := "BCDE grade~"
+		grade = "BCDF grade~"
 	}
-	fmt.Println(inputScore)
+	fmt.Println("You got", grade)
 
 }
