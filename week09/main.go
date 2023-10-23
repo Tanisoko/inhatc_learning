e7488688ed0cf6741ce0f7f80fb16f6198e7e6b9
Tanisoko <jy0317310@naver.com>
2023-10-23 14:56:53 +0900

    week09 v0.3

--- week09/main.go
+++ week09/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
-func double(n int) {
-	n = n * 2
+func double(n *int) {
+	*n = *n * 2
+}
+
+func swap(n1 int, n2 int) {
+	temp := n1
+	n1 = n2
+	n2 = temp
 }
 
 func main() {
 	var a int = 6
-	double(a)
+	double(&a)
 	fmt.Println(a)
+
+	b := 10
+	c := &b
+	fmt.Printf("%T\n", c)
+
+	swap(b, c)
+	fmt.Printf(b, c)
 
 }
